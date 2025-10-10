8b31a5a90bd8b64acc961ae6a6f5e0833ef04c78
mesuit <makatech92@gmail.com>
2025-10-10 12:42:07 +0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./styles/output.css";
+import "react-toastify/dist/ReactToastify.css"; // ✅ add this
 
 const root = createRoot(document.getElementById("root"));
 root.render(
