5105d1808e333f89848b825a478b6b00b4f2e82c
mesuit <makatech92@gmail.com>
2025-10-14 01:36:26 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,23 @@
-import React from "react";
-import { Routes, Route, useLocation } from "react-router-dom";
-import Navbar from "./components/Navbar";
-
-// 🌍 Public Pages
+import Landing from "./pages/Landing";
 import Home from "./pages/Home";
-import Categories from "./pages/Categories";
-import CategoryPage from "./pages/CategoryPage";
-import Submit from "./pages/Submit";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Learn from "./pages/Learn";
-import Earn from "./pages/Earn";
-
-// 🔒 Admin
-import AdminRoute from "./components/AdminRoute";
-import AdminLayout from "./admin/AdminLayout";
-import AdminUsers from "./admin/AdminUsers";
-import AdminPayments from "./admin/AdminPayments";
-import AdminSubmissions from "./admin/AdminSubmissions";
-import AdminAssignments from "./admin/AdminAssignments";
-import AdminUpdates from "./admin/AdminUpdates";
-
-function LayoutWithNavbar({ children }) {
-  const location = useLocation();
-  const isAdminPage = location.pathname.startsWith("/admin");
-
-  return (
-    <div className="min-h-screen bg-gray-50">
-      {!isAdminPage && <Navbar />}
-      <main className="container mx-auto px-4 py-8">{children}</main>
-    </div>
-  );
-}
+// ...other imports
 
 export default function App() {
   return (
     <LayoutWithNavbar>
       <Routes>
         {/* 🌍 Public Routes */}
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Landing />} />       {/* First landing page */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+
+        {/* Logged-in user routes */}
+        <Route path="/home" element={<Home />} />
+        <Route path="/learn" element={<Learn />} />
+        <Route path="/earn" element={<Earn />} />
         <Route path="/categories" element={<Categories />} />
         <Route path="/category/:id" element={<CategoryPage />} />
         <Route path="/submit" element={<Submit />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/learn" element={<Learn />} />
-        <Route path="/earn" element={<Earn />} />
 
         {/* 🔒 Admin Routes */}
         <Route
